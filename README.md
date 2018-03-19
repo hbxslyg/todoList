9e139dcb578dd529e7f6fde80704f1cb3449adda
hbxslyg <hbxslyg@163.com>
2018-03-19 18:00:49 +0800

    第一版-打包编译

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # todoList
-React Demo
+
+[展示页](./dist/index.html)
+
+### 技术栈
+	HTML
+	CSS3
+	JavaScript(ES5,ES6)
+	React(原生)
+	Less
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './container/toduList';
+import './css/style.less'
 
 ReactDOM.render(
     <div className="app">
+        <div className="todo-bgtxt">TODOS</div>
         <App/>
     </div>
     ,
